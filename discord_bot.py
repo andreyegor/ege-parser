9d207c78938758187ca846075d07c4341e6f6848
andreyegor <doesnoexist@gmail.com>
2023-06-05 19:34:46 +0700

    add console output

--- discord_bot.py
+++ discord_bot.py
@@ -28,29 +28,36 @@ async def check(ctx):
             if out['tabel'] == old_tabel.read():
                 await ctx.send('всё как было', file = discord.File("out.png"))
                 logging.info(f"проверка: нет новых данных")
+                print(f"проверка: нет новых данных")
             else:
                 await ctx.send('чёт новое', file = discord.File("out.png"))
                 logging.info(f"проверка: новые данные")
+                print(f"проверка: новые данные")
                 flag = True
         if flag:
             with open('tabel.html','w') as table:
                 table.write(out['tabel'])
     else:
         logging.error('проверка: кака ято ошибка')
+        print('проверка: кака ято ошибка')
         await ctx.send('Ошибка')
     
 @bot.command(pass_context=True)        
 async def start(ctx, arg1, arg2):
-    minutes, silent = int(arg1), arg2 == 'True'
-    logging.info(f"Начало цикла проверки каждые {minutes} минут в {'тихом режиме' if silent else 'обычном режиме'}")
-    if minutes != None and silent != None:
-        check_site.start(ctx, minutes, silent)
-        await ctx.send(f"Проверка баллов каждые {minutes} минут в {'тихом режиме' if silent else 'обычном режиме'}")
+    try:
+        minutes, silent = int(arg1), arg2 == 'True'
+    except:
+         await ctx.send(f"чёт вввёл криво")
+    logging.info(f"начало цикла проверки каждые {minutes} минут в {'тихом режиме' if silent else 'обычном режиме'}")
+    print(f"начало цикла проверки каждые {minutes} минут в {'тихом режиме' if silent else 'обычном режиме'}")
+    check_site.start(ctx, minutes, silent)
+    await ctx.send(f"Проверка баллов каждые {minutes} минут в {'тихом режиме' if silent else 'обычном режиме'}")
 
 @bot.command(pass_context=False)
 async def stop(ctx):
     check_site.stop()
     logging.info('цикл остановлен')
+    print('цикл остановлен')
     await ctx.send('цикл остановлен')  
     
 @tasks.loop(seconds=1)
@@ -63,10 +70,12 @@ async def check_site(ctx, minutes, silent):
             if out['tabel'] == old_tabel.read():
                 if not silent: await ctx.send('всё как было', file = discord.File("out.png"))
                 logging.info(f"цикличная проверка: нет новых данных")
+                print(f"цикличная проверка: нет новых данных")
             else:
                 await ctx.send('вау новое появилось', file = discord.File("out.png"))
                 await ctx.send('🙃', file = discord.File(out['image']))
                 logging.info(f"цикличная проверка: новые данные")
+                print(f"цикличная проверка: новые данные")
                 flag = True
         
         if flag:
@@ -75,6 +84,7 @@ async def check_site(ctx, minutes, silent):
     else:
         check_site.stop()
         logging.error('цикличная проверка: кака ято ошибка')
+        print('цикличная проверка: кака ято ошибка')
         await ctx.send('Ошибка')
 
 
